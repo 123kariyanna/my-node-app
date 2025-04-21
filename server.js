ea82f0ed049febf10afe79e76aa26b9a91808e5e
123kariyanna <101164496+123kariyanna@users.noreply.github.com>
2025-04-21 10:56:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('🚀 node js app sucessfully deloyed in local host!');
+  res.send('🚀 Node.js app successfully deployed in localhost!');
 });
 
-app.listen(port, () => {
+// Change this line to bind to all interfaces (not just localhost)
+app.listen(port, '0.0.0.0', () => {
   console.log(`App running on http://localhost:${port}`);
 });
 
